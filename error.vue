f0fb1770077c9480e723e8346d244aca1fcd70a1
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2023-09-29 01:34:50 +0200

    Check2

--- error.vue
+++ error.vue
@@ -7,15 +7,5 @@
 const props = defineProps({
   error: Object,
 });
-
-// if (props.error.statusCode == 410) {
-//   showError({ message: "Gone", statusCode: 410 });
-//   throw createError({ message: "Gone", statusCode: 410 });
-// }
-
-// if (props.error.statusCode == 404) {
-//   showError({ message: "Page not found", statusCode: 404 });
-// }
-
 const handleError = () => clearError({ redirect: "/" });
 </script>
