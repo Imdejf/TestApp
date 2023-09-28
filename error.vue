be426f7e9335b246eb6d92866a9c645040fa6219
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2023-09-29 01:27:08 +0200

    Check

---
+++ error.vue
@@ -0,0 +1,21 @@
+<template>
+  {{ error.statusCode }}
+  <button @click="handleError">Clear errors</button>
+</template>
+
+<script setup>
+const props = defineProps({
+  error: Object,
+});
+
+// if (props.error.statusCode == 410) {
+//   showError({ message: "Gone", statusCode: 410 });
+//   throw createError({ message: "Gone", statusCode: 410 });
+// }
+
+// if (props.error.statusCode == 404) {
+//   showError({ message: "Page not found", statusCode: 404 });
+// }
+
+const handleError = () => clearError({ redirect: "/" });
+</script>
---
+++ pages/[slug].vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const route = useRoute();
+
+if (route.path == "/dwa") {
+  showError({ message: "Gone", statusCode: 410, fatal: true });
+}
+
+const error404 = () => {
+  throw createError({
+    statusCode: 404,
+    message: "not found",
+    fatal: true,
+  });
+};
+</script>
+<template>
+  <button @click="error404">Error404</button>
+  <button @click="error410">Error410</button>
+</template>
