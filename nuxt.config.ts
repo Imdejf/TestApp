541793227181733fa1bc5c5c974c3787cd38515f
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2023-09-29 01:59:59 +0200

    Check4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  ssr:true,
+  nitro: {
+      prerender: {
+          crawlLinks:true
+      },
+  },
+  hooks: {
+    async 'nitro:config'(nitroConfig) {
+        if (nitroConfig.dev) {
+            return
+        }
+        return
+    }
+},
 })
