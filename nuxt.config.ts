bc8ab6dd45b6ab5b4e660ffa04f1599281b56876
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2023-10-26 10:51:35 +0200

    test code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr:false,
-  target: 'universal',
   nitro: {
-      preset: 'azure',
       prerender: {
           crawlLinks:true,
       },
